319029b4d87fa01d46e265eb6748e1a0ea5400ff
Kyle Houts <kylehouts@macbook-pro-23.local>
2018-08-16 10:23:02 -0500

    Initial Commit

---
+++ Guessing Game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Guessing Game
+//
+//  Created by Kyle Houts on 8/16/18.
+//  Copyright © 2018 Kyle Houts. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
